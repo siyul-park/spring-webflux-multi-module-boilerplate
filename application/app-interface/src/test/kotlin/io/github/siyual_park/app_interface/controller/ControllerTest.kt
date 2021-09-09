b39b294a905d490df4276eba939b896d9affbb93
ara.bak <siyual.bak@gmail.com>
2021-09-09 14:42:37 +0900

    refactoring

--- application/app-interface/src/test/kotlin/io/github/siyual_park/app_interface/controller/ControllerTest.kt
+++ application/app-interface/src/test/kotlin/io/github/siyual_park/app_interface/controller/ControllerTest.kt
@@ -1,6 +1,8 @@
 package io.github.siyual_park.app_interface.controller
 
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
 
-@WebFluxTest
+@SpringBootTest
+@AutoConfigureWebTestClient
 annotation class ControllerTest
