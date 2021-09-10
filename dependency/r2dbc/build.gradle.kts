2a530ae288da29aad144e54d69ed6e31f5ba4e4e
ara.bak <siyual.bak@gmail.com>
2021-09-10 13:34:38 +0900

    fix error

--- dependency/r2dbc/build.gradle.kts
+++ dependency/r2dbc/build.gradle.kts
@@ -11,7 +11,7 @@ dependencies {
     api("org.springframework.boot:spring-boot-starter-data-r2dbc")
 
     runtimeOnly("com.h2database:h2")
-    runtimeOnly("io.r2dbc:r2dbc-h2")
+    api("io.r2dbc:r2dbc-h2")
 }
 
 tasks {
