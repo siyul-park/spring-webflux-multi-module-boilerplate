aa1d9d2166170cd9cee381a7710b4f5bb81ac36e
Ara Park <kdPark0723@gmail.com>
2021-10-02 10:38:30 +0900

    feat: delete user (#10)
    * add user remove
    
    * add user remove
    
    * remove to delete
    
    * refactoring
    
    * refactoring
    
    * feat: remove unused handling

--- application/external/src/main/kotlin/io/github/siyual_park/application/external/handler/VilificationExceptionHandler.kt
+++ application/external/src/main/kotlin/io/github/siyual_park/application/external/handler/VilificationExceptionHandler.kt
@@ -1,6 +1,5 @@
 package io.github.siyual_park.application.external.handler
 
-import com.fasterxml.jackson.module.kotlin.MissingKotlinParameterException
 import io.github.siyual_park.application.external.exception.BadRequestException
 import org.springframework.core.Ordered
 import org.springframework.core.annotation.Order
@@ -13,12 +12,6 @@ import org.springframework.web.bind.support.WebExchangeBindException
 @RestControllerAdvice
 @Order(Ordered.HIGHEST_PRECEDENCE)
 class VilificationExceptionHandler {
-
-    @ExceptionHandler(MissingKotlinParameterException::class)
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    fun handle(exception: MissingKotlinParameterException) {
-        throw BadRequestException(exception.message)
-    }
 
     @ExceptionHandler(WebExchangeBindException::class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
