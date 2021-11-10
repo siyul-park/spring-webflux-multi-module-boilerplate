2dfea9b73ba7f9e5974f2f5776b95c0b26f7edb9
ara.bak <siyual.bak@gmail.com>
2021-11-10 10:33:04 +0900

    refactoring

--- module/auth/src/main/kotlin/io/github/siyual_park/auth/domain/authorization/PrincipalHasScopeAuthorizeProcessor.kt
+++ module/auth/src/main/kotlin/io/github/siyual_park/auth/domain/authorization/PrincipalHasScopeAuthorizeProcessor.kt
@@ -1,13 +1,12 @@
 package io.github.siyual_park.auth.domain.authorization
 
 import io.github.siyual_park.auth.domain.Principal
-import io.github.siyual_park.auth.domain.authentication.AuthenticateMapping
 import io.github.siyual_park.auth.domain.hasScope
 import io.github.siyual_park.auth.entity.ScopeToken
 import org.springframework.stereotype.Component
 
 @Component
-@AuthenticateMapping(filterBy = AllAuthorizeFilter::class)
+@AuthorizeMapping(filterBy = AllAuthorizeFilter::class)
 class PrincipalHasScopeAuthorizeProcessor : AuthorizeProcessor<Principal> {
     override suspend fun authorize(principal: Principal, scopeToken: ScopeToken, targetDomainObject: Any?): Boolean {
         return principal.hasScope(scopeToken)
