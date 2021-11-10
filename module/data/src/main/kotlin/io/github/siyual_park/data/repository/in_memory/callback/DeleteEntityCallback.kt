56f681f6028acd32c18a6c4997f0461346ef8e0c
ara.bak <siyual.bak@gmail.com>
2021-11-10 10:56:24 +0900

    remove unused

--- module/data/src/main/kotlin/io/github/siyual_park/data/repository/in_memory/callback/DeleteEntityCallback.kt
+++
@@ -1,5 +0,0 @@
-package io.github.siyual_park.data.repository.in_memory.callback
-
-interface DeleteEntityCallback<T> {
-    fun onDelete(entity: T)
-}
--- module/data/src/main/kotlin/io/github/siyual_park/data/repository/in_memory/callback/EntityCallbacks.kt
+++
@@ -1,3 +0,0 @@
-package io.github.siyual_park.data.repository.in_memory.callback
-
-interface EntityCallbacks<T> : CreateEntityCallback<T>, UpdateEntityCallback<T>, DeleteEntityCallback<T>
