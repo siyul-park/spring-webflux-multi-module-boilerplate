03c41e3a419d447ca21f6b1fc00ec43b609d16e6
ara.bak <siyual.bak@gmail.com>
2021-11-10 09:38:41 +0900

    refactoring

--- module/auth/src/main/kotlin/io/github/siyual_park/auth/domain/token/TokenIssuer.kt
+++ module/auth/src/main/kotlin/io/github/siyual_park/auth/domain/token/TokenIssuer.kt
@@ -2,15 +2,15 @@ package io.github.siyual_park.auth.domain.token
 
 import io.github.siyual_park.auth.domain.Principal
 import io.github.siyual_park.auth.domain.hasScope
+import io.github.siyual_park.auth.domain.scope_token.ScopeTokenFinder
 import io.github.siyual_park.auth.exception.RequiredPermissionException
-import io.github.siyual_park.auth.repository.ScopeTokenRepository
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
 import java.time.Duration
 
 @Component
 class TokenIssuer(
-    private val scopeTokenRepository: ScopeTokenRepository,
+    private val scopeTokenFinder: ScopeTokenFinder,
     private val tokenEncoder: TokenEncoder,
     private val claimEmbedder: ClaimEmbedder,
     @Value("#{T(java.time.Duration).ofSeconds(\${application.auth.access-token.age})}")
@@ -19,8 +19,8 @@ class TokenIssuer(
     private val refreshTokenAge: Duration = Duration.ofDays(30)
 ) {
     suspend fun issue(principal: Principal): Tokens {
-        val accessTokenCreateScope = scopeTokenRepository.findByNameOrFail("access-token:create")
-        val refreshTokenCreateScope = scopeTokenRepository.findByNameOrFail("refresh-token:create")
+        val accessTokenCreateScope = scopeTokenFinder.findByNameOrFail("access-token:create", cache = true)
+        val refreshTokenCreateScope = scopeTokenFinder.findByNameOrFail("refresh-token:create", cache = true)
 
         if (!principal.hasScope(accessTokenCreateScope)) {
             throw RequiredPermissionException()
@@ -28,15 +28,16 @@ class TokenIssuer(
 
         val claim = claimEmbedder.embedding(principal)
 
-        val accessTokenScope = principal.scope.filter { it.id != accessTokenCreateScope.id && it.id != refreshTokenCreateScope.id }
-        val refreshTokenScope = principal.scope.filter { it.id != refreshTokenCreateScope.id }
-
+        val accessTokenScope =
+            principal.scope.filter { it.id != accessTokenCreateScope.id && it.id != refreshTokenCreateScope.id }
         val accessToken = tokenEncoder.encode(
             claim,
             accessTokenAge,
             scope = accessTokenScope
         )
+
         val refreshToken = if (principal.hasScope(refreshTokenCreateScope)) {
+            val refreshTokenScope = principal.scope.filter { it.id != refreshTokenCreateScope.id }
             tokenEncoder.encode(
                 claim,
                 refreshTokenAge,
