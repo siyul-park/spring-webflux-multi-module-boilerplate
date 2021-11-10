8a18094878f4cca11e6b9101a2e574d6350f65de
ara.bak <siyual.bak@gmail.com>
2021-11-10 16:43:44 +0900

    refactoring

--- domain/user/src/main/kotlin/io/github/siyual_park/user/entity/UserCredential.kt
+++ domain/user/src/main/kotlin/io/github/siyual_park/user/entity/UserCredential.kt
@@ -2,16 +2,12 @@ package io.github.siyual_park.user.entity
 
 import io.github.siyual_park.data.TimeableEntity
 import io.github.siyual_park.data.copyDefaultColumn
-import org.springframework.data.relational.core.mapping.Column
 import org.springframework.data.relational.core.mapping.Table
 
 @Table("user_credentials")
 data class UserCredential(
-    @Column("user_id")
     var userId: Long,
-
     var password: String,
-    @Column("hash_algorithm")
     var hashAlgorithm: String,
 ) : TimeableEntity<UserCredential, Long>() {
     override fun clone(): UserCredential {
