c0df5f4703dc7748d2486ec43fdd08994703b941
ara.bak <siyual.bak@gmail.com>
2021-11-10 14:23:28 +0900

    refactoring

--- module/data/src/main/kotlin/io/github/siyual_park/data/repository/r2dbc/CachedR2DBCRepository.kt
+++ module/data/src/main/kotlin/io/github/siyual_park/data/repository/r2dbc/CachedR2DBCRepository.kt
@@ -14,14 +14,30 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
 import org.springframework.data.domain.Sort
+import org.springframework.data.r2dbc.core.R2dbcEntityOperations
 import org.springframework.data.relational.core.query.CriteriaDefinition
+import reactor.core.scheduler.Scheduler
+import reactor.core.scheduler.Schedulers
+import kotlin.reflect.KClass
 import kotlin.reflect.full.memberProperties
 
 @Suppress("UNCHECKED_CAST")
 class CachedR2DBCRepository<T : Cloneable<T>, ID : Any> private constructor(
     private val repository: R2DBCRepository<T, ID>,
-    override val storage: Storage<T, ID>,
-) : R2DBCRepository<T, ID>, CachedRepository<T, ID> by SimpleCachedRepository(repository, storage) {
+    cacheBuilder: CacheBuilder<ID, T>,
+) : R2DBCRepository<T, ID>,
+    CachedRepository<T, ID> by SimpleCachedRepository(
+        repository,
+        Storage(
+            cacheBuilder,
+            object : Extractor<T, ID> {
+                override fun getKey(entity: T): ID {
+                    return repository.entityManager.getId(entity)
+                }
+            }
+        )
+    ) {
+
     override val entityManager: EntityManager<T, ID>
         get() = repository.entityManager
 
@@ -99,18 +115,18 @@ class CachedR2DBCRepository<T : Cloneable<T>, ID : Any> private constructor(
             repository: R2DBCRepository<T, ID>,
             cacheBuilder: CacheBuilder<Any, Any>
         ): CachedR2DBCRepository<T, ID> {
-            val entityManager = repository.entityManager
+            return CachedR2DBCRepository(repository, cacheBuilder as CacheBuilder<ID, T>)
+        }
 
+        fun <T : Cloneable<T>, ID : Any> of(
+            entityOperations: R2dbcEntityOperations,
+            clazz: KClass<T>,
+            cacheBuilder: CacheBuilder<Any, Any>,
+            scheduler: Scheduler = Schedulers.boundedElastic()
+        ): CachedR2DBCRepository<T, ID> {
             return CachedR2DBCRepository(
-                repository,
-                Storage(
-                    cacheBuilder as CacheBuilder<ID, T>,
-                    object : Extractor<T, ID> {
-                        override fun getKey(entity: T): ID {
-                            return entityManager.getId(entity)
-                        }
-                    }
-                )
+                SimpleR2DBCRepository(entityOperations, clazz, scheduler),
+                cacheBuilder as CacheBuilder<ID, T>
             )
         }
     }
