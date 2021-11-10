5ead22fa5db4dcd45883e9e5bcbe86c321c29b99
ara.bak <siyual.bak@gmail.com>
2021-11-10 09:24:39 +0900

    refactoring

--- module/auth/src/main/kotlin/io/github/siyual_park/auth/spring/ScopeEvaluator.kt
+++ module/auth/src/main/kotlin/io/github/siyual_park/auth/spring/ScopeEvaluator.kt
@@ -3,6 +3,7 @@ package io.github.siyual_park.auth.spring
 import io.github.siyual_park.auth.domain.Principal
 import io.github.siyual_park.auth.domain.scope_token.ScopeEvaluator
 import io.github.siyual_park.auth.domain.scope_token.ScopeTokenFinder
+import io.github.siyual_park.auth.entity.ScopeToken
 import kotlinx.coroutines.flow.toSet
 import kotlinx.coroutines.runBlocking
 import org.springframework.security.access.PermissionEvaluator
@@ -23,27 +24,8 @@ class ScopeEvaluator(
             }
 
             try {
-                val scope = when (permission) {
-                    is String -> {
-                        setOf(scopeTokenFinder.findByNameOrFail(permission, cache = true))
-                    }
-                    is Collection<*> -> {
-                        scopeTokenFinder.findAllByName(permission.map { it.toString() }, cache = true).toSet()
-                    }
-                    else -> {
-                        return@runBlocking false
-                    }
-                }
-
-                val adjustedTargetDomainObject = if (targetDomainObject is Array<*>) {
-                    if (targetDomainObject.size == 2 && targetDomainObject[1]?.javaClass?.name == "kotlin.reflect.full.KCallables\$callSuspend\$1") {
-                        targetDomainObject[0]
-                    } else {
-                        targetDomainObject
-                    }
-                } else {
-                    targetDomainObject
-                }
+                val scope = getScope(permission) ?: return@runBlocking false
+                val adjustedTargetDomainObject = adjustTargetDomainObject(targetDomainObject)
 
                 return@runBlocking scopeEvaluator.evaluate(principal, adjustedTargetDomainObject, scope)
             } catch (e: Exception) {
@@ -56,8 +38,34 @@ class ScopeEvaluator(
         authentication: Authentication?,
         targetId: Serializable?,
         targetType: String?,
-        permission: Any?
+        permission: Any
     ): Boolean {
         return false
+    }
+
+    private suspend fun getScope(permission: Any): Set<ScopeToken>? {
+        return when (permission) {
+            is String -> {
+                setOf(scopeTokenFinder.findByNameOrFail(permission, cache = true))
+            }
+            is Collection<*> -> {
+                scopeTokenFinder.findAllByName(permission.map { it.toString() }, cache = true).toSet()
+            }
+            else -> {
+                null
+            }
+        }
+    }
+
+    private suspend fun adjustTargetDomainObject(targetDomainObject: Any?): Any? {
+        return if (targetDomainObject is Array<*>) {
+            if (targetDomainObject.size == 2 && targetDomainObject[1]?.javaClass?.name == "kotlin.reflect.full.KCallables\$callSuspend\$1") {
+                targetDomainObject[0]
+            } else {
+                targetDomainObject
+            }
+        } else {
+            targetDomainObject
+        }
     }
 }
