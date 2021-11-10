03d0e78885279212211a3515c79efd4bd2c28af2
ara.bak <siyual.bak@gmail.com>
2021-11-10 20:20:10 +0900

    init package

---
+++ module/event/build.gradle.kts
@@ -0,0 +1,15 @@
+dependencies {
+    implementation(project(":dependency:async"))
+    testImplementation(project(":dependency:async-test"))
+
+    implementation(project(":dependency:r2dbc"))
+
+    implementation(project(":module:data"))
+    testImplementation(project(":module:data-test"))
+}
+
+kotlin.sourceSets["main"].kotlin.srcDirs("src/main")
+kotlin.sourceSets["test"].kotlin.srcDirs("src/test")
+
+sourceSets["main"].resources.srcDirs("src/main/resources")
+sourceSets["test"].resources.srcDirs("src/test/resources")
