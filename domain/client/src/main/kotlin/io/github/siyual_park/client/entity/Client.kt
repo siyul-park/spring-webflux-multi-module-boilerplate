35d2d48d2ec0f15d426a378bb3f2580aae971277
ara.bak <siyual.bak@gmail.com>
2021-11-11 15:24:10 +0900

    add migration

--- domain/client/src/main/kotlin/io/github/siyual_park/client/entity/Client.kt
+++ domain/client/src/main/kotlin/io/github/siyual_park/client/entity/Client.kt
@@ -1,9 +1,11 @@
 package io.github.siyual_park.client.entity
 
 import io.github.siyual_park.data.TimeableEntity
+import io.github.siyual_park.data.annotation.GeneratedValue
 import io.github.siyual_park.data.copyDefaultColumn
 import org.springframework.data.relational.core.mapping.Table
 import java.net.URI
+import java.time.Instant
 
 @Table("clients")
 data class Client(
@@ -11,6 +13,8 @@ data class Client(
     val grantTypes: Collection<String>,
     val redirectUris: Collection<URI>,
     val tokenEndpointAuthMethod: TokenEndpointAuthMethod,
+    @GeneratedValue
+    var deletedAt: Instant? = null
 ) : TimeableEntity<Client, Long>(), ClientEntity {
     override val clientId: Long
         get() = id!!
---
+++ domain/client/src/main/kotlin/io/github/siyual_park/client/migration/CreateClient.kt
@@ -0,0 +1,53 @@
+package io.github.siyual_park.client.migration
+
+import io.github.siyual_park.data.migration.Migration
+import io.github.siyual_park.data.migration.createUpdatedAtTrigger
+import io.github.siyual_park.data.migration.dropTable
+import io.github.siyual_park.data.migration.fetchSQL
+import io.github.siyual_park.data.migration.isDriver
+import org.springframework.data.r2dbc.core.R2dbcEntityOperations
+
+class CreateClient : Migration {
+    private val tableName = "clients"
+
+    override suspend fun up(entityOperations: R2dbcEntityOperations) {
+        if (entityOperations.isDriver("PostgreSQL")) {
+            entityOperations.fetchSQL(
+                "CREATE TABLE $tableName" +
+                    "(" +
+                    "id SERIAL PRIMARY KEY, " +
+
+                    "name VARCHAR(64), " +
+                    "grant_types VARCHAR(64)[] NOT NULL, " +
+                    "redirect_uris VARCHAR(256)[] NOT NULL, " +
+                    "token_endpoint_auth_method VARCHAR(64) NOT NULL, " +
+
+                    "created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
+                    "updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
+                    "deleted_at TIMESTAMP" +
+                    ")"
+            )
+            entityOperations.createUpdatedAtTrigger(tableName)
+        } else {
+            entityOperations.fetchSQL(
+                "CREATE TABLE $tableName" +
+                    "(" +
+                    "id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY, " +
+
+                    "name VARCHAR(64), " +
+                    "grant_types VARCHAR(64)[] NOT NULL, " +
+                    "redirect_uris VARCHAR(256)[] NOT NULL, " +
+                    "token_endpoint_auth_method VARCHAR(64) NOT NULL, " +
+
+                    "created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP, " +
+                    "updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, " +
+                    "deleted_at TIMESTAMP" +
+                    ")"
+            )
+        }
+    }
+
+    override suspend fun down(entityOperations: R2dbcEntityOperations) {
+        entityOperations.dropTable(tableName)
+    }
+}
