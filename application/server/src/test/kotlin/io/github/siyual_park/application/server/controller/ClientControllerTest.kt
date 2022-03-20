2f858fa5548744d199d1c94b8a3899ffe9941bab
siyual.bak <siyual.bak@gmail.com>
2022-03-20 20:12:49 +0900

    refactor: ClientControllerTest 메서드 명 수정

--- application/server/src/test/kotlin/io/github/siyual_park/application/server/controller/ClientControllerTest.kt
+++ application/server/src/test/kotlin/io/github/siyual_park/application/server/controller/ClientControllerTest.kt
@@ -7,22 +7,15 @@ import io.github.siyual_park.application.server.dummy.DummyCreateUserPayload
 import io.github.siyual_park.application.server.dummy.RandomNameFactory
 import io.github.siyual_park.application.server.gateway.ClientControllerGateway
 import io.github.siyual_park.application.server.gateway.GatewayAuthorization
-import io.github.siyual_park.auth.domain.scope_token.ScopeTokenFinder
-import io.github.siyual_park.auth.entity.ScopeToken
-import io.github.siyual_park.auth.entity.ids
-import io.github.siyual_park.auth.repository.ScopeTokenRepository
 import io.github.siyual_park.client.domain.ClientFactory
 import io.github.siyual_park.client.domain.ClientScopeFinder
-import io.github.siyual_park.client.domain.auth.ClientPrincipal
 import io.github.siyual_park.client.domain.auth.ClientPrincipalExchanger
 import io.github.siyual_park.client.entity.ClientType
 import io.github.siyual_park.spring.test.CoroutineTest
 import io.github.siyual_park.spring.test.IntegrationTest
 import io.github.siyual_park.user.domain.UserFactory
-import io.github.siyual_park.user.domain.auth.UserPrincipal
 import io.github.siyual_park.user.domain.auth.UserPrincipalExchanger
 import io.github.siyual_park.util.Presence
-import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.flow.toList
 import kotlinx.coroutines.flow.toSet
 import kotlinx.coroutines.reactive.asFlow
@@ -34,34 +27,25 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 
 @IntegrationTest
-class ClientControllerTest@Autowired constructor(
+class ClientControllerTest @Autowired constructor(
     private val gatewayAuthorization: GatewayAuthorization,
     private val clientControllerGateway: ClientControllerGateway,
     private val userFactory: UserFactory,
     private val clientFactory: ClientFactory,
     private val userPrincipalExchanger: UserPrincipalExchanger,
     private val clientPrincipalExchanger: ClientPrincipalExchanger,
-    private val clientScopeFinder: ClientScopeFinder,
-    private val scopeTokenFinder: ScopeTokenFinder,
-    private val scopeTokenRepository: ScopeTokenRepository
+    private val clientScopeFinder: ClientScopeFinder
 ) : CoroutineTest() {
+
     @Test
-    fun testCreateSuccess() = blocking {
+    fun `POST clients, status = 201`() = blocking {
         val principal = DummyCreateClientPayload.create()
             .let { clientFactory.create(it) }
             .let { clientPrincipalExchanger.exchange(it) }
 
-        val additionalScope = scopeTokenRepository.findByNameOrFail("clients:create")
-
-        val scope = mutableSetOf<ScopeToken>()
-        scope.add(additionalScope)
-        scope.addAll(principal.scope)
-
         gatewayAuthorization.setPrincipal(
-            ClientPrincipal(
-                id = principal.id,
-                scope = scope
-            )
+            principal,
+            push = listOf("clients:create")
         )
 
         ClientType.values().forEach {
@@ -91,22 +75,14 @@ class ClientControllerTest@Autowired constructor(
     }
 
     @Test
-    fun testCreateFail() = blocking {
+    fun `POST clients, status = 409`() = blocking {
         val principal = DummyCreateClientPayload.create()
             .let { clientFactory.create(it) }
             .let { clientPrincipalExchanger.exchange(it) }
 
-        val additionalScope = scopeTokenRepository.findByNameOrFail("clients:create")
-
-        val scope = mutableSetOf<ScopeToken>()
-        scope.add(additionalScope)
-        scope.addAll(principal.scope)
-
         gatewayAuthorization.setPrincipal(
-            ClientPrincipal(
-                id = principal.id,
-                scope = scope
-            )
+            principal,
+            push = listOf("clients:create")
         )
 
         val request = DummyCreateClientRequest.create()
@@ -117,7 +93,21 @@ class ClientControllerTest@Autowired constructor(
     }
 
     @Test
-    fun testReadAllSuccess() = blocking {
+    fun `POST clients, status = 403`() = blocking {
+        val principal = DummyCreateClientPayload.create()
+            .let { clientFactory.create(it) }
+            .let { clientPrincipalExchanger.exchange(it) }
+
+        gatewayAuthorization.setPrincipal(principal)
+
+        val request = DummyCreateClientRequest.create()
+        val response = clientControllerGateway.create(request)
+
+        assertEquals(HttpStatus.FORBIDDEN, response.status)
+    }
+
+    @Test
+    fun `GET clients, status = 201`() = blocking {
         val principal = DummyCreateUserPayload.create()
             .let { userFactory.create(it) }
             .let { userPrincipalExchanger.exchange(it) }
@@ -149,7 +139,7 @@ class ClientControllerTest@Autowired constructor(
     }
 
     @Test
-    fun testReadAllFail() = blocking {
+    fun `GET clients, status = 403`() = blocking {
         val principal = DummyCreateUserPayload.create()
             .let { userFactory.create(it) }
             .let { userPrincipalExchanger.exchange(it) }
@@ -157,15 +147,9 @@ class ClientControllerTest@Autowired constructor(
         val client = DummyCreateClientPayload.create()
             .let { clientFactory.create(it) }
 
-        val removeScope = scopeTokenRepository.findByNameOrFail("clients:read")
-
         gatewayAuthorization.setPrincipal(
-            UserPrincipal(
-                id = principal.id,
-                scope = scopeTokenFinder.findAllWithResolved(principal.scope.ids())
-                    .filter { it.id != removeScope.id }
-                    .toSet()
-            )
+            principal,
+            pop = listOf("clients:read")
         )
 
         val response = clientControllerGateway.readAll(
@@ -178,7 +162,7 @@ class ClientControllerTest@Autowired constructor(
     }
 
     @Test
-    fun testReadSelfSuccessWhenUseClient() = blocking {
+    fun `GET clients_self, status = 200, when use client principal`() = blocking {
         val payload = DummyCreateClientPayload.create()
         val client = clientFactory.create(payload)
         val principal = clientPrincipalExchanger.exchange(client)
@@ -200,7 +184,7 @@ class ClientControllerTest@Autowired constructor(
     }
 
     @Test
-    fun testReadSelfSuccessWhenUseUser() = blocking {
+    fun `GET clients_self, status = 200, when use user principal`() = blocking {
         val client = DummyCreateClientPayload.create()
             .let { clientFactory.create(it) }
         val user = DummyCreateUserPayload.create()
@@ -225,20 +209,14 @@ class ClientControllerTest@Autowired constructor(
     }
 
     @Test
-    fun testReadSelfFail() = blocking {
+    fun `GET clients_self, status = 403`() = blocking {
         val principal = DummyCreateClientPayload.create()
             .let { clientFactory.create(it) }
             .let { clientPrincipalExchanger.exchange(it) }
 
-        val removeScope = scopeTokenRepository.findByNameOrFail("clients[self]:read")
-
         gatewayAuthorization.setPrincipal(
-            ClientPrincipal(
-                id = principal.id,
-                scope = scopeTokenFinder.findAllWithResolved(principal.scope.ids())
-                    .filter { it.id != removeScope.id }
-                    .toSet()
-            )
+            principal,
+            pop = listOf("clients[self]:read")
         )
 
         val response = clientControllerGateway.readSelf()
@@ -247,23 +225,16 @@ class ClientControllerTest@Autowired constructor(
     }
 
     @Test
-    fun testUpdateSelfSuccess() = blocking {
+    fun `PATCH clients_self, status = 200`() = blocking {
         val payload = DummyCreateClientPayload.create()
         val client = clientFactory.create(payload)
         val principal = clientPrincipalExchanger.exchange(client)
 
-        val additionalScope = scopeTokenRepository.findByNameOrFail("clients[self]:update")
-
-        val scope = mutableSetOf<ScopeToken>()
-        scope.add(additionalScope)
-        scope.addAll(principal.scope)
-
         gatewayAuthorization.setPrincipal(
-            ClientPrincipal(
-                id = principal.id,
-                scope = scope
-            )
+            principal,
+            push = listOf("clients[self]:update")
         )
+
         val request = MutableClientData(
             name = RandomNameFactory.create(10),
             origin = client.origin
@@ -283,23 +254,16 @@ class ClientControllerTest@Autowired constructor(
     }
 
     @Test
-    fun testDeleteSelfSuccess() = blocking {
+    fun `DELETE clients_self, status = 200`() = blocking {
         val payload = DummyCreateClientPayload.create()
         val client = clientFactory.create(payload)
         val principal = clientPrincipalExchanger.exchange(client)
 
-        val additionalScope = scopeTokenRepository.findByNameOrFail("clients[self]:delete")
-
-        val scope = mutableSetOf<ScopeToken>()
-        scope.add(additionalScope)
-        scope.addAll(principal.scope)
-
         gatewayAuthorization.setPrincipal(
-            ClientPrincipal(
-                id = principal.id,
-                scope = scope
-            )
+            principal,
+            push = listOf("clients[self]:delete")
         )
+
         val response = clientControllerGateway.deleteSelf()
 
         assertEquals(HttpStatus.NO_CONTENT, response.status)
@@ -309,7 +273,7 @@ class ClientControllerTest@Autowired constructor(
     }
 
     @Test
-    fun testReadSuccess() = blocking {
+    fun `GET clients_{client_id}, status = 200`() = blocking {
         val payload = DummyCreateClientPayload.create()
         val client = clientFactory.create(payload)
         val principal = clientPrincipalExchanger.exchange(client)
@@ -334,7 +298,7 @@ class ClientControllerTest@Autowired constructor(
     }
 
     @Test
-    fun testUpdateSuccess() = blocking {
+    fun `PATCH clients_{client_id}, status = 200`() = blocking {
         val principal = DummyCreateClientPayload.create()
             .let { clientFactory.create(it) }
             .let { clientPrincipalExchanger.exchange(it) }
@@ -342,17 +306,9 @@ class ClientControllerTest@Autowired constructor(
         val otherClient = DummyCreateClientPayload.create()
             .let { clientFactory.create(it) }
 
-        val additionalScope = scopeTokenRepository.findByNameOrFail("clients:update")
-
-        val scope = mutableSetOf<ScopeToken>()
-        scope.add(additionalScope)
-        scope.addAll(principal.scope)
-
         gatewayAuthorization.setPrincipal(
-            ClientPrincipal(
-                id = principal.id,
-                scope = scope
-            )
+            principal,
+            push = listOf("clients:update")
         )
 
         val request = MutableClientData(
@@ -374,7 +330,7 @@ class ClientControllerTest@Autowired constructor(
     }
 
     @Test
-    fun testDeleteSuccess() = blocking {
+    fun `DELEATE clients_{client_id}, status = 200`() = blocking {
         val payload = DummyCreateClientPayload.create()
         val client = clientFactory.create(payload)
         val principal = clientPrincipalExchanger.exchange(client)
@@ -382,18 +338,11 @@ class ClientControllerTest@Autowired constructor(
         val otherPayload = DummyCreateClientPayload.create()
         val otherClient = clientFactory.create(otherPayload)
 
-        val additionalScope = scopeTokenRepository.findByNameOrFail("clients:delete")
-
-        val scope = mutableSetOf<ScopeToken>()
-        scope.add(additionalScope)
-        scope.addAll(principal.scope)
-
         gatewayAuthorization.setPrincipal(
-            ClientPrincipal(
-                id = principal.id,
-                scope = scope
-            )
+            principal,
+            push = listOf("clients:delete")
         )
+
         val response = clientControllerGateway.delete(otherClient.id!!)
 
         assertEquals(HttpStatus.NO_CONTENT, response.status)
