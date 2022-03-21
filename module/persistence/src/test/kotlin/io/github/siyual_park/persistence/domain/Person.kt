1009fee87a53a1fa06bbc7f1c83d8921f8ac8563
siyual.bak <siyual.bak@gmail.com>
2022-03-21 21:56:00 +0900

    fix: proxy 수정

--- module/persistence/src/test/kotlin/io/github/siyual_park/persistence/domain/Person.kt
+++ module/persistence/src/test/kotlin/io/github/siyual_park/persistence/domain/Person.kt
@@ -6,9 +6,9 @@ import io.github.siyual_park.persistence.entity.PersonData
 import io.github.siyual_park.persistence.proxy
 
 class Person(
-    root: PersonData,
+    value: PersonData,
     repository: Repository<PersonData, Long>
-) : Persistence<PersonData, Long>(root, repository) {
+) : Persistence<PersonData, Long>(value, repository) {
     val id by proxy(root, PersonData::id)
     var name by proxy(root, PersonData::name)
     var age by proxy(root, PersonData::age)
