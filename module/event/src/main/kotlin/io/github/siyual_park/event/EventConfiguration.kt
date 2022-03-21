cba5de58dff273380365679119e573f9d6d1ed13
siyual.bak <siyual.bak@gmail.com>
2022-03-21 22:05:10 +0900

    refactor: 클래스 합치기

--- module/event/src/main/kotlin/io/github/siyual_park/event/EventConfiguration.kt
+++ module/event/src/main/kotlin/io/github/siyual_park/event/EventConfiguration.kt
@@ -1,22 +1,35 @@
 package io.github.siyual_park.event
 
+import org.springframework.beans.BeansException
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class EventConfiguration(
-    private val applicationContext: ApplicationContext,
-    private val eventFilterFactory: EventFilterFactory
+    private val applicationContext: ApplicationContext
 ) {
     @Autowired(required = true)
     fun configEventEmitter(eventEmitter: EventEmitter) {
         applicationContext.getBeansOfType(EventConsumer::class.java).values.forEach {
             it.javaClass.annotations.filterIsInstance<Subscribe>()
                 .forEach { annotation ->
-                    val filter = eventFilterFactory.create(annotation)
+                    val filter = createFilter(annotation)
                     eventEmitter.on(filter, it)
                 }
+        }
+    }
+
+    private fun createFilter(mapping: Subscribe): EventFilter {
+        val filterBeen = try {
+            applicationContext.getBean(mapping.filterBy.java)
+        } catch (e: BeansException) {
+            TypeMatchEventFilter(mapping.filterBy)
+        }
+        return if (filterBeen is EventFilter) {
+            filterBeen
+        } else {
+            TypeMatchEventFilter(mapping.filterBy)
         }
     }
 }
