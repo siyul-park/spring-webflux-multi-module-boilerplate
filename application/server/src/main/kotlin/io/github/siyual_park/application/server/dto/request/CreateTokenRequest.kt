9a3f487d153f40eb9fc45f5161301c9524c62d04
siyual.bak <siyual.bak@gmail.com>
2022-03-23 09:40:08 +0900

    feat: swagger 문서 수정

--- application/server/src/main/kotlin/io/github/siyual_park/application/server/dto/request/CreateTokenRequest.kt
+++ application/server/src/main/kotlin/io/github/siyual_park/application/server/dto/request/CreateTokenRequest.kt
@@ -2,20 +2,29 @@ package io.github.siyual_park.application.server.dto.request
 
 import com.fasterxml.jackson.annotation.JsonIgnore
 import io.github.siyual_park.application.server.dto.GrantType
+import io.swagger.annotations.ApiModelProperty
 import javax.validation.constraints.AssertTrue
 
 data class CreateTokenRequest(
+    @ApiModelProperty(name = "grant_type")
     val grantType: GrantType,
+    @ApiModelProperty(name = "scope")
     val scope: String? = null,
 
+    @ApiModelProperty(name = "username")
     val username: String? = null,
+    @ApiModelProperty(name = "password")
     val password: String? = null,
 
+    @ApiModelProperty(name = "refresh_token")
     val refreshToken: String? = null,
 
+    @ApiModelProperty(name = "client_id")
     val clientId: Long,
+    @ApiModelProperty(name = "client_secret")
     val clientSecret: String? = null
 ) {
+    @ApiModelProperty(hidden = true)
     @JsonIgnore
     @AssertTrue
     fun isValid(): Boolean {
--- application/server/src/main/kotlin/io/github/siyual_park/application/server/dto/request/GrantScopeRequest.kt
+++ application/server/src/main/kotlin/io/github/siyual_park/application/server/dto/request/GrantScopeRequest.kt
@@ -1,6 +1,7 @@
 package io.github.siyual_park.application.server.dto.request
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import io.swagger.annotations.ApiModelProperty
 import javax.validation.constraints.AssertTrue
 import javax.validation.constraints.Size
 
@@ -9,6 +10,7 @@ data class GrantScopeRequest(
     @Size(max = 64)
     val name: String? = null,
 ) {
+    @ApiModelProperty(hidden = true)
     @JsonIgnore
     @AssertTrue
     fun isValid(): Boolean {
