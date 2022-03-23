d4053fc400ce1414d91bccdab5f3a34fd264f183
siyual.bak <siyual.bak@gmail.com>
2022-03-23 12:14:01 +0900

    feat: Deprecated 제거

--- module/data/src/main/kotlin/io/github/siyual_park/data/repository/r2dbc/EntityManager.kt
+++ module/data/src/main/kotlin/io/github/siyual_park/data/repository/r2dbc/EntityManager.kt
@@ -1,19 +1,30 @@
 package io.github.siyual_park.data.repository.r2dbc
 
+import org.springframework.dao.InvalidDataAccessResourceUsageException
 import org.springframework.data.r2dbc.core.R2dbcEntityOperations
+import org.springframework.data.r2dbc.dialect.DialectResolver
 import org.springframework.data.r2dbc.mapping.OutboundRow
+import org.springframework.data.r2dbc.query.UpdateMapper
+import org.springframework.data.r2dbc.support.ArrayUtils
 import org.springframework.data.relational.core.mapping.RelationalPersistentEntity
+import org.springframework.data.relational.core.mapping.RelationalPersistentProperty
 import org.springframework.data.relational.core.sql.SqlIdentifier
 import org.springframework.data.util.ProxyUtils
+import org.springframework.r2dbc.core.Parameter
+import org.springframework.util.CollectionUtils
 import kotlin.reflect.KClass
 
+@Suppress("UNCHECKED_CAST")
 class EntityManager<T : Any, ID : Any>(
     entityOperations: R2dbcEntityOperations,
     val clazz: KClass<T>,
 ) {
-    private val databaseClient = entityOperations.databaseClient
-    private val dataAccessStrategy = entityOperations.dataAccessStrategy
-    private val mappingContext = dataAccessStrategy.converter.mappingContext
+    private val client = entityOperations.databaseClient
+    private val converter = entityOperations.converter
+    private val dialect = DialectResolver.getDialect(client.connectionFactory)
+    private val updateManager = UpdateMapper(dialect, converter)
+
+    private val mappingContext = converter.mappingContext
 
     val idColumn: SqlIdentifier
     val idProperty: String
@@ -22,7 +33,7 @@ class EntityManager<T : Any, ID : Any>(
         val persistentEntity = getRequiredEntity(clazz.java)
 
         idColumn = persistentEntity.requiredIdProperty.columnName
-        idProperty = dataAccessStrategy.toSql(idColumn)
+        idProperty = updateManager.toSql(idColumn)
     }
 
     fun getId(row: OutboundRow): ID {
@@ -35,7 +46,63 @@ class EntityManager<T : Any, ID : Any>(
     }
 
     fun getOutboundRow(entity: T): OutboundRow {
-        return dataAccessStrategy.getOutboundRow(entity)
+        val row = OutboundRow()
+
+        converter.write(entity, row)
+
+        val persistentEntity = getRequiredEntity(entity)
+        for (property in persistentEntity) {
+            val value = row[property.columnName]
+            if (value.value != null && shouldConvertArrayValue(property, value)) {
+                val writeValue = getArrayValue(value, property)
+                row[property.columnName] = writeValue
+            }
+        }
+
+        return row
+    }
+
+    private fun shouldConvertArrayValue(property: RelationalPersistentProperty, value: Parameter): Boolean {
+        if (!property.isCollectionLike) {
+            return false
+        }
+        if (value.hasValue() && (value.value is Collection<*> || value.value!!.javaClass.isArray)) {
+            return true
+        }
+        return MutableCollection::class.java.isAssignableFrom(value.type) || value.type.isArray
+    }
+
+    private fun getArrayValue(value: Parameter, property: RelationalPersistentProperty): Parameter {
+        if (value.type == ByteArray::class.java) {
+            return value
+        }
+        val arrayColumns = dialect.arraySupport
+        if (!arrayColumns.isSupported) {
+            throw InvalidDataAccessResourceUsageException("Dialect ${dialect.javaClass.name} does not support array columns")
+        }
+        var actualType: Class<*>? = null
+        if (value.value is Collection<*>) {
+            actualType = CollectionUtils.findCommonElementType((value.value as Collection<*>?)!!)
+        } else if (!value.isEmpty && value.value!!.javaClass.isArray) {
+            actualType = value.value!!.javaClass.componentType
+        }
+        if (actualType == null) {
+            actualType = property.actualType
+        }
+        actualType = converter.getTargetType(actualType)
+        if (value.isEmpty) {
+            val targetType = arrayColumns.getArrayType(actualType)
+            val depth = if (actualType.isArray) ArrayUtils.getDimensionDepth(actualType) else 1
+            val targetArrayType = ArrayUtils.getArrayClass(targetType, depth)
+            return Parameter.empty(targetArrayType)
+        }
+        return Parameter.fromOrEmpty(
+            converter.getArrayValue(
+                arrayColumns, property,
+                value.value!!
+            ),
+            actualType
+        )
     }
 
     fun getRequiredEntity(entity: T): RelationalPersistentEntity<T> {
