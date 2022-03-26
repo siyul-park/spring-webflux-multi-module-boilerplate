cb0dee3eb25d13434d495143790b2a71a40a74cc
siyual.bak <siyual.bak@gmail.com>
2022-03-26 19:50:35 +0900

    refactor: 메서드명 수정

--- application/server/src/main/kotlin/io/github/siyual_park/application/server/configuration/ScopeTokenConfiguration.kt
+++ application/server/src/main/kotlin/io/github/siyual_park/application/server/configuration/ScopeTokenConfiguration.kt
@@ -7,6 +7,7 @@ import org.springframework.boot.context.event.ApplicationReadyEvent
 import org.springframework.context.annotation.Configuration
 import org.springframework.context.event.EventListener
 import org.springframework.core.annotation.Order
+import org.springframework.dao.DuplicateKeyException
 
 @Configuration
 class ScopeTokenConfiguration(
@@ -20,52 +21,53 @@ class ScopeTokenConfiguration(
         val publicClientScope = scopeTokenFactory.upsert(name = "public(client):pack")
         val confidentialClientScope = scopeTokenFactory.upsert(name = "confidential(client):pack")
 
-        scopeTokenFactory.upsert(name = "token:create").also { grant(it, listOf(confidentialClientScope, publicClientScope)) }
-        scopeTokenFactory.upsert(name = "access-token:create").also { grant(it, listOf(userScope, confidentialClientScope, publicClientScope)) }
-        scopeTokenFactory.upsert(name = "refresh-token:create").also { grant(it, listOf(userScope)) }
+        scopeTokenFactory.upsert(name = "token:create").also { granted(it, listOf(confidentialClientScope, publicClientScope)) }
+        scopeTokenFactory.upsert(name = "access-token:create").also { granted(it, listOf(userScope, confidentialClientScope, publicClientScope)) }
+        scopeTokenFactory.upsert(name = "refresh-token:create").also { granted(it, listOf(userScope)) }
 
-        scopeTokenFactory.upsert(name = "principal[self]:read").also { grant(it, listOf(userScope, confidentialClientScope, publicClientScope)) }
+        scopeTokenFactory.upsert(name = "principal[self]:read").also { granted(it, listOf(userScope, confidentialClientScope, publicClientScope)) }
 
-        scopeTokenFactory.upsert(name = "users:create").also { grant(it, listOf(confidentialClientScope, publicClientScope)) }
-        scopeTokenFactory.upsert(name = "users:read").also { grant(it, listOf(userScope, confidentialClientScope)) }
+        scopeTokenFactory.upsert(name = "users:create").also { granted(it, listOf(confidentialClientScope, publicClientScope)) }
+        scopeTokenFactory.upsert(name = "users:read").also { granted(it, listOf(userScope, confidentialClientScope)) }
         scopeTokenFactory.upsert(name = "users:update")
         scopeTokenFactory.upsert(name = "users:delete")
-        scopeTokenFactory.upsert(name = "users[self]:read").also { grant(it, listOf(userScope)) }
-        scopeTokenFactory.upsert(name = "users[self]:update").also { grant(it, listOf(userScope)) }
-        scopeTokenFactory.upsert(name = "users[self]:delete").also { grant(it, listOf(userScope)) }
+        scopeTokenFactory.upsert(name = "users[self]:read").also { granted(it, listOf(userScope)) }
+        scopeTokenFactory.upsert(name = "users[self]:update").also { granted(it, listOf(userScope)) }
+        scopeTokenFactory.upsert(name = "users[self]:delete").also { granted(it, listOf(userScope)) }
 
         scopeTokenFactory.upsert(name = "clients:create")
         scopeTokenFactory.upsert(name = "clients:read")
         scopeTokenFactory.upsert(name = "clients:update")
         scopeTokenFactory.upsert(name = "clients:delete")
-        scopeTokenFactory.upsert(name = "clients[self]:read").also { grant(it, listOf(userScope, confidentialClientScope, publicClientScope)) }
-        scopeTokenFactory.upsert(name = "clients[self]:update").also { grant(it, listOf(confidentialClientScope)) }
-        scopeTokenFactory.upsert(name = "clients[self]:delete").also { grant(it, listOf(confidentialClientScope)) }
+        scopeTokenFactory.upsert(name = "clients[self]:read").also { granted(it, listOf(userScope, confidentialClientScope, publicClientScope)) }
+        scopeTokenFactory.upsert(name = "clients[self]:update").also { granted(it, listOf(confidentialClientScope)) }
+        scopeTokenFactory.upsert(name = "clients[self]:delete").also { granted(it, listOf(confidentialClientScope)) }
 
         scopeTokenFactory.upsert(name = "users.scope:create")
         scopeTokenFactory.upsert(name = "users.scope:read")
         scopeTokenFactory.upsert(name = "users.scope:delete")
-        scopeTokenFactory.upsert(name = "users[self].scope:read").also { grant(it, listOf(userScope)) }
+        scopeTokenFactory.upsert(name = "users[self].scope:read").also { granted(it, listOf(userScope)) }
 
         scopeTokenFactory.upsert(name = "clients.scope:create")
         scopeTokenFactory.upsert(name = "clients.scope:read")
         scopeTokenFactory.upsert(name = "clients.scope:delete")
-        scopeTokenFactory.upsert(name = "clients[self].scope:read").also { grant(it, listOf(userScope, confidentialClientScope, publicClientScope)) }
+        scopeTokenFactory.upsert(name = "clients[self].scope:read").also { granted(it, listOf(userScope, confidentialClientScope, publicClientScope)) }
 
         scopeTokenFactory.upsert(name = "scope:create")
-        scopeTokenFactory.upsert(name = "scope:read").also { grant(it, listOf(userScope, confidentialClientScope, publicClientScope)) }
+        scopeTokenFactory.upsert(name = "scope:read").also { granted(it, listOf(userScope, confidentialClientScope, publicClientScope)) }
         scopeTokenFactory.upsert(name = "scope:update")
         scopeTokenFactory.upsert(name = "scope:delete")
 
-        scopeTokenFactory.upsert(name = "scope.children:read").also { grant(it, listOf(userScope, confidentialClientScope, publicClientScope)) }
+        scopeTokenFactory.upsert(name = "scope.children:read").also { granted(it, listOf(userScope, confidentialClientScope, publicClientScope)) }
         scopeTokenFactory.upsert(name = "scope.children:create")
         scopeTokenFactory.upsert(name = "scope.children:delete")
     }
 
-    private suspend fun grant(child: ScopeToken, parents: List<ScopeToken>) {
+    private suspend fun granted(child: ScopeToken, parents: List<ScopeToken>) {
         parents.forEach {
-            if (!it.has(child)) {
+            try {
                 it.grant(child)
+            } catch (_: DuplicateKeyException) {
             }
         }
     }
