5b8be0a60cfec4086df37d020145b16fb9564a5f
siyual.bak <siyual.bak@gmail.com>
2022-04-16 19:25:30 +0900

    feat: proxyNotNull 를 Id 에서 사용

--- domain/client/src/main/kotlin/io/github/siyual_park/client/domain/ClientCredential.kt
+++ domain/client/src/main/kotlin/io/github/siyual_park/client/domain/ClientCredential.kt
@@ -5,13 +5,14 @@ import io.github.siyual_park.client.repository.ClientCredentialRepository
 import io.github.siyual_park.event.EventPublisher
 import io.github.siyual_park.persistence.Persistence
 import io.github.siyual_park.persistence.proxy
+import io.github.siyual_park.persistence.proxyNotNull
 
 class ClientCredential(
     value: ClientCredentialData,
     clientCredentialRepository: ClientCredentialRepository,
     eventPublisher: EventPublisher
 ) : Persistence<ClientCredentialData, Long>(value, clientCredentialRepository, eventPublisher) {
-    val id by proxy(root, ClientCredentialData::id)
+    val id by proxyNotNull(root, ClientCredentialData::id)
     val clientId by proxy(root, ClientCredentialData::clientId)
 
     fun checkSecret(secret: String): Boolean {
--- domain/user/src/main/kotlin/io/github/siyual_park/user/domain/UserCredential.kt
+++ domain/user/src/main/kotlin/io/github/siyual_park/user/domain/UserCredential.kt
@@ -4,6 +4,7 @@ import io.github.siyual_park.auth.domain.hash
 import io.github.siyual_park.event.EventPublisher
 import io.github.siyual_park.persistence.Persistence
 import io.github.siyual_park.persistence.proxy
+import io.github.siyual_park.persistence.proxyNotNull
 import io.github.siyual_park.user.entity.UserCredentialData
 import io.github.siyual_park.user.repository.UserCredentialRepository
 import java.security.MessageDigest
@@ -13,7 +14,7 @@ class UserCredential(
     userCredentialRepository: UserCredentialRepository,
     eventPublisher: EventPublisher
 ) : Persistence<UserCredentialData, Long>(value, userCredentialRepository, eventPublisher) {
-    val id by proxy(root, UserCredentialData::id)
+    val id by proxyNotNull(root, UserCredentialData::id)
     val userId by proxy(root, UserCredentialData::userId)
 
     private val hashAlgorithm by proxy(root, UserCredentialData::hashAlgorithm)
