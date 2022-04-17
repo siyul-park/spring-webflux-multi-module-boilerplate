722b46394d81b5a2e363da795f86fbd558ba0545
siyual.bak <siyual.bak@gmail.com>
2022-04-18 02:30:27 +0900

    feat: converter 수정

--- module/data/src/main/kotlin/io/github/siyual_park/data/converter/BinaryToBytesConverter.kt
+++ module/data/src/main/kotlin/io/github/siyual_park/data/converter/BinaryToBytesConverter.kt
@@ -1,14 +1,14 @@
 package io.github.siyual_park.data.converter
 
+import io.github.siyual_park.data.annotation.ConverterScope
 import org.bson.types.Binary
 import org.springframework.core.convert.converter.Converter
 import org.springframework.data.convert.ReadingConverter
-import org.springframework.data.convert.WritingConverter
 import org.springframework.stereotype.Component
 
 @Component
-@WritingConverter
 @ReadingConverter
+@ConverterScope(ConverterScope.Type.MONGO)
 class BinaryToBytesConverter : Converter<Binary, ByteArray> {
     override fun convert(source: Binary): ByteArray {
         return source.data
