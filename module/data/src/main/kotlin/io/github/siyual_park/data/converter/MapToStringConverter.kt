d6b70ccfad940c8f1fdfe8d149077b1730f3f32c
siyual.bak <siyual.bak@gmail.com>
2022-04-18 22:55:56 +0900

    feat: not mapping map to string mongo

--- module/data/src/main/kotlin/io/github/siyual_park/data/converter/MapToStringConverter.kt
+++ module/data/src/main/kotlin/io/github/siyual_park/data/converter/MapToStringConverter.kt
@@ -1,12 +1,14 @@
 package io.github.siyual_park.data.converter
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.github.siyual_park.data.annotation.ConverterScope
 import org.springframework.core.convert.converter.Converter
 import org.springframework.data.convert.WritingConverter
 import org.springframework.stereotype.Component
 
 @Component
 @WritingConverter
+@ConverterScope(ConverterScope.Type.R2DBC)
 class MapToStringConverter(
     private val objectMapper: ObjectMapper
 ) : Converter<Map<String, Any>, String> {
--- module/data/src/main/kotlin/io/github/siyual_park/data/converter/StringToMapConverter.kt
+++ module/data/src/main/kotlin/io/github/siyual_park/data/converter/StringToMapConverter.kt
@@ -2,12 +2,14 @@ package io.github.siyual_park.data.converter
 
 import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.github.siyual_park.data.annotation.ConverterScope
 import org.springframework.core.convert.converter.Converter
 import org.springframework.data.convert.ReadingConverter
 import org.springframework.stereotype.Component
 
 @Component
 @ReadingConverter
+@ConverterScope(ConverterScope.Type.R2DBC)
 class StringToMapConverter(
     private val objectMapper: ObjectMapper
 ) : Converter<String, Map<String, Any>> {
