fc6a1a9b4024e5879291c88393a1f57902686d21
siyual.bak <siyual.bak@gmail.com>
2022-04-19 15:33:08 +0900

    feat: remove _class in mongo

---
+++ module/data/src/main/kotlin/io/github/siyual_park/data/configuration/MappingMongoConverterConfiguration.kt
@@ -0,0 +1,15 @@
+package io.github.siyual_park.data.configuration
+
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter
+
+@Configuration
+class MappingMongoConverterConfiguration(
+    private val mappingMongoConverter: MappingMongoConverter
+) : InitializingBean {
+    override fun afterPropertiesSet() {
+        mappingMongoConverter.setTypeMapper(DefaultMongoTypeMapper(null))
+    }
+}
