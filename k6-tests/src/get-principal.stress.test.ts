0acf5f5b2b9db08c2fb96047ed15d9c9ab08b2d9
siyual.bak <siyual.bak@gmail.com>
2022-04-23 17:43:43 +0900

    test: vus -> 10000

--- k6-tests/src/get-principal.stress.test.ts
+++ k6-tests/src/get-principal.stress.test.ts
@@ -5,14 +5,15 @@ import client from './client';
 
 export const options: Options = {
   stages: [
-    { duration: '2m', target: 200 },
-    { duration: '5m', target: 200 },
-    { duration: '2m', target: 500 },
-    { duration: '5m', target: 500 },
-    { duration: '2m', target: 2000 },
-    { duration: '5m', target: 2000 },
-    { duration: '2m', target: 3000 },
+    { duration: '2m', target: 4000 },
     { duration: '5m', target: 4000 },
+    { duration: '2m', target: 6000 },
+    { duration: '5m', target: 6000 },
+    { duration: '2m', target: 8000 },
+    { duration: '5m', target: 8000 },
+    { duration: '2m', target: 10000 },
+    { duration: '5m', target: 10000 },
+    { duration: '10m', target: 0 },
   ],
   thresholds: {
     'http_req_duration{type:POST_token}': ['max>=0'],
