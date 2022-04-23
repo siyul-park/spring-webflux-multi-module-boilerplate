32f47c0fae2b34ce8c64af10cd34a28263937a91
siyual.bak <siyual.bak@gmail.com>
2022-04-23 11:56:23 +0900

    test: patch-users_{user_id}.test

--- performance-test/src/dummy/dummy-update-user-request.ts
+++ performance-test/src/dummy/dummy-update-user-request.ts
@@ -1,14 +1,13 @@
 // @ts-ignore
 import { randomString } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
-import { CreateUserRequest } from '../request';
+import { UpdateUserRequest } from '../request';
 
-function dummyCreateUserRequest(): CreateUserRequest {
+function dummyUpdateUserRequest(): UpdateUserRequest {
     return {
         name: randomString(10),
         email: `${randomString(10)}@test.com`,
-        password: randomString(10),
     }
 }
 
-export default dummyCreateUserRequest;
+export default dummyUpdateUserRequest;
--- performance-test/src/dummy/index.ts
+++ performance-test/src/dummy/index.ts
@@ -2,3 +2,4 @@ export { default as dummyCreateClientRequest } from './dummy-create-client-reque
 export { default as dummyUpdateClientRequest } from './dummy-update-client-request';
 
 export { default as dummyCreateUserRequest } from './dummy-create-user-request';
+export { default as dummyUpdateUserRequest } from './dummy-update-user-request';
