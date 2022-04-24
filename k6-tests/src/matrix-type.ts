ceddee5c74a5f84102aa92542aef7e529964e6bd
siyual.bak <siyual.bak@gmail.com>
2022-04-24 11:10:07 +0900

    test: 지표 추가

--- k6-tests/src/matrix-type.ts
+++ k6-tests/src/matrix-type.ts
@@ -3,12 +3,12 @@ import { Options } from 'k6/options';
 function matrixType(options: Options, types: string[]): void {
   types.forEach((type) => {
     const thresholdNames = [
-      `http_req_duration{scenario:${type}}`,
-      `http_req_failed{scenario:${type}}`,
-      `http_req_receiving{scenario:${type}}`,
-      `http_req_sending{scenario:${type}}`,
-      `http_req_waiting{scenario:${type}}`,
-      `http_reqs{scenario:${type}}`,
+      `http_req_duration{type:${type}}`,
+      `http_req_failed{type:${type}}`,
+      `http_req_receiving{type:${type}}`,
+      `http_req_sending{type:${type}}`,
+      `http_req_waiting{type:${type}}`,
+      `http_reqs{type:${type}}`,
     ];
     thresholdNames.forEach((thresholdName) => {
       if (options.thresholds == null) {
