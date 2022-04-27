b55054f2d11c4e4b88b558e2c8fa9547b475179a
siyual.bak <siyual.bak@gmail.com>
2022-04-27 23:14:15 +0900

    docs: apply model resolver from object mapper

--- application/server/src/main/kotlin/io/github/siyual_park/application/server/configuration/SwaggerConfiguration.kt
+++ application/server/src/main/kotlin/io/github/siyual_park/application/server/configuration/SwaggerConfiguration.kt
@@ -1,7 +1,13 @@
 package io.github.siyual_park.application.server.configuration
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinFeature
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.SingletonSupport
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.github.siyual_park.search.pagination.OffsetPage
 import io.github.siyual_park.ulid.ULID
+import io.swagger.v3.core.jackson.ModelResolver
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.security.SecurityScheme
@@ -19,6 +25,22 @@ class SwaggerConfiguration {
             .replaceWithClass(Duration::class.java, Long::class.java)
             .replaceWithClass(ULID::class.java, String::class.java)
             .replaceWithClass(OffsetPage::class.java, List::class.java)
+    }
+
+    @Bean
+    fun modelResolver(objectMapper: ObjectMapper): ModelResolver {
+        return ModelResolver(
+            jacksonObjectMapper().registerModule(
+                KotlinModule.Builder()
+                    .withReflectionCacheSize(512)
+                    .configure(KotlinFeature.NullToEmptyCollection, false)
+                    .configure(KotlinFeature.NullToEmptyMap, false)
+                    .configure(KotlinFeature.NullIsSameAsDefault, false)
+                    .configure(KotlinFeature.SingletonSupport, false)
+                    .configure(KotlinFeature.StrictNullChecks, false)
+                    .build()
+            ).setPropertyNamingStrategy(objectMapper.propertyNamingStrategy)
+        )
     }
 
     @Bean
