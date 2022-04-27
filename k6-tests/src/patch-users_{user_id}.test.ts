bf1f863d823dbb830e074a0dc0101bc96168208b
siyual.bak <siyual.bak@gmail.com>
2022-04-27 21:38:00 +0900

    test: change patch user error

--- k6-tests/src/patch-users_{user_id}.test.ts
+++ k6-tests/src/patch-users_{user_id}.test.ts
@@ -1,7 +1,6 @@
 import { Options } from 'k6/options';
 
 import { UserGateway } from './gateway';
-import { UserInfo } from './response';
 import { dummyCreateUserRequest, dummyUpdateUserRequest } from './dummy';
 
 import client from './client';
@@ -14,28 +13,13 @@ export const options: Options = {
 
 matrixType(options, ['POST_token', 'POST_users', 'PATCH_users_id']);
 
-export function setup() {
-  const userGateway = new UserGateway({
-    grantType: 'client_credentials',
-    clientId: client.id,
-    clientSecret: client.secret,
-  });
-  const createUserRequest = dummyCreateUserRequest();
-  const user = userGateway.create(createUserRequest);
+const userGateway = new UserGateway({
+  grantType: 'client_credentials',
+  clientId: client.id,
+  clientSecret: client.secret,
+});
 
-  return {
-    ...user,
-    ...createUserRequest,
-  };
-}
-
-export default (user: UserInfo & { password: string }) => {
-  const userGateway = new UserGateway({
-    grantType: 'password',
-    clientId: client.id,
-    clientSecret: client.secret,
-    username: user.name,
-    password: user.password,
-  });
+export default () => {
+  const user = userGateway.create(dummyCreateUserRequest());
   userGateway.update(user.id, dummyUpdateUserRequest());
 };
