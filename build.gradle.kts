8bc3d382efb3b0e79afb82d9f320e98b988cb77f
Ara Park <siyual.bak@gmail.com>
2022-05-08 18:56:14 +0900

    feat: jacoco (#73)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 val kotlin_version: String by project
 val junit_version: String by project
+val jacoco_version: String by project
 val coroutines_version: String by project
 val projectreactor_version: String by project
 val reactor_kotlin_extensions_version: String by project
@@ -30,14 +31,16 @@ plugins {
 
     kotlin("jvm")
     id("org.jlleitschuh.gradle.ktlint")
+    id("jacoco")
 
     id("org.springframework.boot")
     id("io.spring.dependency-management")
     kotlin("plugin.spring")
 }
 
-group = "io.github.siyual-park"
-version = "0.0.0-SNAPSHOT"
+jacoco {
+    toolVersion = jacoco_version
+}
 
 tasks {
     bootJar {
@@ -49,9 +52,13 @@ tasks {
     }
 }
 
+group = "io.github.siyual-park"
+version = "0.0.0-SNAPSHOT"
+
 allprojects {
     apply(plugin = "kotlin")
     apply(plugin = "org.jlleitschuh.gradle.ktlint")
+    apply(plugin = "jacoco")
 
     apply(plugin = "org.springframework.boot")
     apply(plugin = "io.spring.dependency-management")
@@ -130,5 +137,43 @@ allprojects {
     }
     tasks.withType<Test> {
         useJUnitPlatform()
+    }
+}
+
+// task to gather code coverage from multiple subprojects
+// NOTE: the `JacocoReport` tasks do *not* depend on the `test` task by default. Meaning you have to ensure
+// that `test` (or other tasks generating code coverage) run before generating the report.
+// You can achieve this by calling the `test` lifecycle task manually
+// $ ./gradlew test codeCoverageReport
+tasks.register<JacocoReport>("codeCoverageReport") {
+    // If a subproject applies the 'jacoco' plugin, add the result it to the report
+    subprojects {
+        val subproject = this
+        subproject.plugins.withType<JacocoPlugin>().configureEach {
+            subproject.tasks.matching { it.extensions.findByType<JacocoTaskExtension>() != null }.configureEach {
+                val testTask = this
+                sourceSets(subproject.sourceSets.main.get())
+                executionData(testTask)
+            }
+
+            // To automatically run `test` every time `./gradlew codeCoverageReport` is called,
+            // you may want to set up a task dependency between them as shown below.
+            // Note that this requires the `test` tasks to be resolved eagerly (see `forEach`) which
+            // may have a negative effect on the configuration time of your build.
+            subproject.tasks.matching { it.extensions.findByType<JacocoTaskExtension>() != null }.forEach {
+                rootProject.tasks["codeCoverageReport"].dependsOn(it)
+            }
+        }
+    }
+
+    // enable the different report types (html, xml, csv)
+    reports {
+        // xml is usually used to integrate code coverage with
+        // other tools like SonarQube, Coveralls or Codecov
+        xml.required.set(true)
+
+        // HTML reports can be used to see code coverage
+        // without any external tools
+        html.required.set(true)
     }
 }
