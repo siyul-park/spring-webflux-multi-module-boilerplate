5f45245789d6c19e3c71b4a315158b65426177f2
siyual.bak <siyual.bak@gmail.com>
2022-05-10 15:25:55 +0900

    refactor: remove unused class

--- module/data/src/main/kotlin/io/github/siyual_park/data/SoftDeletable.kt
+++
@@ -1,9 +0,0 @@
-package io.github.siyual_park.data
-
-import io.github.siyual_park.data.annotation.GeneratedValue
-import java.time.Instant
-
-interface SoftDeletable {
-    @GeneratedValue
-    var deletedAt: Instant?
-}
