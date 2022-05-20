b70a08c751f5e2a4013c3b62a757b942606fa191
siyual.bak <siyual.bak@gmail.com>
2022-05-20 22:50:29 +0900

    fix: swagger

--- application/server/src/main/kotlin/io/github/siyual_park/application/server/configuration/SwaggerConfiguration.kt
+++ application/server/src/main/kotlin/io/github/siyual_park/application/server/configuration/SwaggerConfiguration.kt
@@ -5,13 +5,10 @@ import com.fasterxml.jackson.module.kotlin.KotlinFeature
 import com.fasterxml.jackson.module.kotlin.KotlinModule
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.github.siyual_park.presentation.pagination.OffsetPage
-import io.github.siyual_park.presentation.project.ProjectNode
 import io.github.siyual_park.ulid.ULID
 import io.swagger.v3.core.jackson.ModelResolver
 import io.swagger.v3.oas.models.Components
 import io.swagger.v3.oas.models.OpenAPI
-import io.swagger.v3.oas.models.media.ArraySchema
-import io.swagger.v3.oas.models.media.StringSchema
 import io.swagger.v3.oas.models.security.SecurityScheme
 import org.springdoc.core.SpringDocUtils.getConfig
 import org.springframework.context.annotation.Bean
@@ -27,7 +24,6 @@ class SwaggerConfiguration {
             .replaceWithClass(Duration::class.java, Long::class.java)
             .replaceWithClass(ULID::class.java, String::class.java)
             .replaceWithClass(OffsetPage::class.java, List::class.java)
-            .replaceWithSchema(ProjectNode::class.java, ArraySchema().apply { items = StringSchema() })
     }
 
     @Bean
