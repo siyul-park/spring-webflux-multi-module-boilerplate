3d05785d1077b7e67d47f2df88e169d951fb05d0
siyual.bak <siyual.bak@gmail.com>
2022-05-21 22:06:09 +0900

    refactor: usedPool

--- module/data/src/main/kotlin/io/github/siyual_park/data/cache/NestedQueryStorage.kt
+++ module/data/src/main/kotlin/io/github/siyual_park/data/cache/NestedQueryStorage.kt
@@ -10,7 +10,12 @@ class NestedQueryStorage<T : Any>(
     override val parent: NestedQueryStorage<T>? = null,
 ) : QueryStorage<T>, GeneralNestedStorage<NestedQueryStorage<T>> {
     private val mutex = Mutex()
-    private val delegator = AsyncLazy { freePool.poll().also { usedPool.add(it) }.also { it.clear() } }
+    private val delegator = AsyncLazy {
+        freePool.poll().also {
+            it.clear()
+            usedPool.add(it)
+        }
+    }
 
     override suspend fun getIfPresent(where: String): T? {
         return parent?.getIfPresent(where) ?: delegator.get().getIfPresent(where)
