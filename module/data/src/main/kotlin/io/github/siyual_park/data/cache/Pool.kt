ffa51195809c3401ee31aaf1cd034ccc0243c549
siyual.bak <siyual.bak@gmail.com>
2022-05-21 20:06:04 +0900

    feat: change to use soft pool

--- module/data/src/main/kotlin/io/github/siyual_park/data/cache/Pool.kt
+++ module/data/src/main/kotlin/io/github/siyual_park/data/cache/Pool.kt
@@ -2,13 +2,14 @@ package io.github.siyual_park.data.cache
 
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
+import org.h2.util.SoftHashMap
 import java.util.Collections
 import java.util.WeakHashMap
 
 class Pool<T : Any>(
     private val load: suspend () -> T
 ) {
-    private val store = Collections.newSetFromMap(WeakHashMap<T, Boolean>())
+    private val store = Collections.newSetFromMap(SoftHashMap<T, Boolean>())
     private val mutex = Mutex()
 
     suspend fun poll(): T {
