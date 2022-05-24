77d2dc9868e56b8ba7a519515b2e173deb356799
siyual.bak <siyual.bak@gmail.com>
2022-05-25 00:30:23 +0900

    feat: add type

--- module/data/src/main/kotlin/io/github/siyual_park/data/criteria/Criteria.kt
+++ module/data/src/main/kotlin/io/github/siyual_park/data/criteria/Criteria.kt
@@ -1,103 +1,102 @@
 package io.github.siyual_park.data.criteria
 
-import kotlin.reflect.KProperty
 import kotlin.reflect.KProperty1
 
-sealed class Criteria {
-    data class And(val value: Collection<Criteria>) : Criteria() {
+sealed class Criteria<T : Any> {
+    data class And<T : Any>(val value: Collection<Criteria<T>>) : Criteria<T>() {
         override fun toString(): String {
             return "(${value.map { it.toString() }.joinToString { " && " }})"
         }
     }
-    data class Or(val value: Collection<Criteria>) : Criteria() {
+    data class Or<T : Any>(val value: Collection<Criteria<T>>) : Criteria<T>() {
         override fun toString(): String {
             return "(${value.map { it.toString() }.joinToString { " || " }})"
         }
     }
 
-    data class Equals<V : Any?>(val key: KProperty<V>, val value: V) : Criteria() {
+    data class Equals<T : Any, V : Any?>(val key: KProperty1<T, V>, val value: V) : Criteria<T>() {
         override fun toString(): String {
             return "${key.name} == $value"
         }
     }
-    data class NotEquals<V : Any?>(val key: KProperty<V>, val value: V) : Criteria() {
+    data class NotEquals<T : Any, V : Any?>(val key: KProperty1<T, V>, val value: V) : Criteria<T>() {
         override fun toString(): String {
             return "${key.name} != $value"
         }
     }
 
-    data class Between<V : Comparable<V>>(val key: KProperty<V?>, val value: ClosedRange<V>) : Criteria() {
+    data class Between<T : Any, V : Comparable<V>>(val key: KProperty1<T, V?>, val value: ClosedRange<V>) : Criteria<T>() {
         override fun toString(): String {
             return "(${key.name} >= ${value.start} && ${key.name} <= ${value.endInclusive})"
         }
     }
-    data class NotBetween<V : Comparable<V>>(val key: KProperty<V?>, val value: ClosedRange<V>) : Criteria() {
+    data class NotBetween<T : Any, V : Comparable<V>>(val key: KProperty1<T, V?>, val value: ClosedRange<V>) : Criteria<T>() {
         override fun toString(): String {
             return "(${key.name} < ${value.start} || ${key.name} > ${value.endInclusive})"
         }
     }
 
-    data class LessThan<V : Comparable<V>>(val key: KProperty<V?>, val value: V) : Criteria() {
+    data class LessThan<T : Any, V : Comparable<V>>(val key: KProperty1<T, V?>, val value: V) : Criteria<T>() {
         override fun toString(): String {
             return "${key.name} < $value"
         }
     }
-    data class LessThanEquals<V : Comparable<V>>(val key: KProperty<V?>, val value: V) : Criteria() {
+    data class LessThanEquals<T : Any, V : Comparable<V>>(val key: KProperty1<T, V?>, val value: V) : Criteria<T>() {
         override fun toString(): String {
             return "${key.name} <= $value"
         }
     }
 
-    data class GreaterThan<V : Comparable<V>>(val key: KProperty<V?>, val value: V) : Criteria() {
+    data class GreaterThan<T : Any, V : Comparable<V>>(val key: KProperty1<T, V?>, val value: V) : Criteria<T>() {
         override fun toString(): String {
             return "${key.name} > $value"
         }
     }
-    data class GreaterThanEquals<V : Comparable<V>>(val key: KProperty<V?>, val value: V) : Criteria() {
+    data class GreaterThanEquals<T : Any, V : Comparable<V>>(val key: KProperty1<T, V?>, val value: V) : Criteria<T>() {
         override fun toString(): String {
             return "${key.name} >= $value"
         }
     }
 
-    data class IsNull<V : Any?>(val key: KProperty<V>) : Criteria() {
+    data class IsNull<T : Any, V : Any?>(val key: KProperty1<T, V>) : Criteria<T>() {
         override fun toString(): String {
             return "${key.name} == null"
         }
     }
-    data class NotNull<V : Any?>(val key: KProperty<V>) : Criteria() {
+    data class NotNull<T : Any, V : Any?>(val key: KProperty1<T, V>) : Criteria<T>() {
         override fun toString(): String {
             return "${key.name} != null"
         }
     }
 
-    data class Like<T : Any>(val key: KProperty1<T, String?>, val value: String) : Criteria() {
+    data class Like<T : Any>(val key: KProperty1<T, String?>, val value: String) : Criteria<T>() {
         override fun toString(): String {
             return "${key.name} like $value"
         }
     }
-    data class NotLike<T : Any>(val key: KProperty1<T, String?>, val value: String) : Criteria() {
+    data class NotLike<T : Any>(val key: KProperty1<T, String?>, val value: String) : Criteria<T>() {
         override fun toString(): String {
             return "${key.name} !like $value"
         }
     }
 
-    data class In<V : Any?>(val key: KProperty<V>, val value: Collection<V>) : Criteria() {
+    data class In<T : Any, V : Any?>(val key: KProperty1<T, V>, val value: Collection<V>) : Criteria<T>() {
         override fun toString(): String {
             return "${key.name} in (${value.map { it.toString() }.joinToString { ", " }})"
         }
     }
-    data class NotIn<V : Any?>(val key: KProperty<V>, val value: Collection<V>) : Criteria() {
+    data class NotIn<T : Any, V : Any?>(val key: KProperty1<T, V>, val value: Collection<V>) : Criteria<T>() {
         override fun toString(): String {
             return "${key.name} not in (${value.map { it.toString() }.joinToString { ", " }})"
         }
     }
 
-    data class IsTrue<T : Any>(val key: KProperty1<T, Boolean?>) : Criteria() {
+    data class IsTrue<T : Any>(val key: KProperty1<T, Boolean?>) : Criteria<T>() {
         override fun toString(): String {
             return "${key.name} == true"
         }
     }
-    data class IsFalse<T : Any>(val key: KProperty1<T, Boolean?>) : Criteria() {
+    data class IsFalse<T : Any>(val key: KProperty1<T, Boolean?>) : Criteria<T>() {
         override fun toString(): String {
             return "${key.name} == false"
         }
--- module/data/src/main/kotlin/io/github/siyual_park/data/criteria/CriteriaParseStrategy.kt
+++
@@ -1,5 +0,0 @@
-package io.github.siyual_park.data.criteria
-
-interface CriteriaParseStrategy<IN : Criteria, OUT : Any?> {
-    fun parse(criteria: IN): OUT
-}
