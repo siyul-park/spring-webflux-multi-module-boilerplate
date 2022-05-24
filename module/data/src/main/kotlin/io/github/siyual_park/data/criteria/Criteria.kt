33f252910d6bea1a84fcf9f30280674daabfbecb
siyual.bak <siyual.bak@gmail.com>
2022-05-25 00:16:35 +0900

    feat: Criteria toString

--- module/data/src/main/kotlin/io/github/siyual_park/data/criteria/Criteria.kt
+++ module/data/src/main/kotlin/io/github/siyual_park/data/criteria/Criteria.kt
@@ -3,30 +3,102 @@ package io.github.siyual_park.data.criteria
 import kotlin.reflect.KProperty1
 
 sealed class Criteria<T : Any> {
-    data class And<T : Any>(val value: Collection<Criteria<T>>) : Criteria<T>()
-    data class Or<T : Any>(val value: Collection<Criteria<T>>) : Criteria<T>()
+    data class And<T : Any>(val value: Collection<Criteria<T>>) : Criteria<T>() {
+        override fun toString(): String {
+            return "(${value.map { it.toString() }.joinToString { " && " }})"
+        }
+    }
+    data class Or<T : Any>(val value: Collection<Criteria<T>>) : Criteria<T>() {
+        override fun toString(): String {
+            return "(${value.map { it.toString() }.joinToString { " || " }})"
+        }
+    }
 
-    data class Equals<T : Any, V : Any?>(val key: KProperty1<T, V>, val value: V) : Criteria<T>()
-    data class NotEquals<T : Any, V : Any?>(val key: KProperty1<T, V>, val value: V) : Criteria<T>()
+    data class Equals<T : Any, V : Any?>(val key: KProperty1<T, V>, val value: V) : Criteria<T>() {
+        override fun toString(): String {
+            return "${key.name} == $value"
+        }
+    }
+    data class NotEquals<T : Any, V : Any?>(val key: KProperty1<T, V>, val value: V) : Criteria<T>() {
+        override fun toString(): String {
+            return "${key.name} != $value"
+        }
+    }
 
-    data class Between<T : Any, V : Comparable<V>>(val key: KProperty1<T, V?>, val value: ClosedRange<V>) : Criteria<T>()
-    data class NotBetween<T : Any, V : Comparable<V>>(val key: KProperty1<T, V?>, val value: ClosedRange<V>) : Criteria<T>()
+    data class Between<T : Any, V : Comparable<V>>(val key: KProperty1<T, V?>, val value: ClosedRange<V>) : Criteria<T>() {
+        override fun toString(): String {
+            return "(${key.name} >= ${value.start} && ${key.name} <= ${value.endInclusive})"
+        }
+    }
+    data class NotBetween<T : Any, V : Comparable<V>>(val key: KProperty1<T, V?>, val value: ClosedRange<V>) : Criteria<T>() {
+        override fun toString(): String {
+            return "(${key.name} < ${value.start} || ${key.name} > ${value.endInclusive})"
+        }
+    }
 
-    data class LessThan<T : Any, V : Comparable<V>>(val key: KProperty1<T, V?>, val value: V) : Criteria<T>()
-    data class LessThanEquals<T : Any, V : Comparable<V>>(val key: KProperty1<T, V?>, val value: V) : Criteria<T>()
+    data class LessThan<T : Any, V : Comparable<V>>(val key: KProperty1<T, V?>, val value: V) : Criteria<T>() {
+        override fun toString(): String {
+            return "${key.name} < $value"
+        }
+    }
+    data class LessThanEquals<T : Any, V : Comparable<V>>(val key: KProperty1<T, V?>, val value: V) : Criteria<T>() {
+        override fun toString(): String {
+            return "${key.name} <= $value"
+        }
+    }
 
-    data class GreaterThan<T : Any, V : Comparable<V>>(val key: KProperty1<T, V?>, val value: V) : Criteria<T>()
-    data class GreaterThanEquals<T : Any, V : Comparable<V>>(val key: KProperty1<T, V?>, val value: V) : Criteria<T>()
+    data class GreaterThan<T : Any, V : Comparable<V>>(val key: KProperty1<T, V?>, val value: V) : Criteria<T>() {
+        override fun toString(): String {
+            return "${key.name} > $value"
+        }
+    }
+    data class GreaterThanEquals<T : Any, V : Comparable<V>>(val key: KProperty1<T, V?>, val value: V) : Criteria<T>() {
+        override fun toString(): String {
+            return "${key.name} >= $value"
+        }
+    }
 
-    data class IsNull<T : Any, V : Any?>(val key: KProperty1<T, V>) : Criteria<T>()
-    data class NotNull<T : Any, V : Any?>(val key: KProperty1<T, V>) : Criteria<T>()
+    data class IsNull<T : Any, V : Any?>(val key: KProperty1<T, V>) : Criteria<T>() {
+        override fun toString(): String {
+            return "${key.name} == null"
+        }
+    }
+    data class NotNull<T : Any, V : Any?>(val key: KProperty1<T, V>) : Criteria<T>() {
+        override fun toString(): String {
+            return "${key.name} != null"
+        }
+    }
 
-    data class Like<T : Any>(val key: KProperty1<T, String?>) : Criteria<T>()
-    data class NotLike<T : Any>(val key: KProperty1<T, String?>) : Criteria<T>()
+    data class Like<T : Any>(val key: KProperty1<T, String?>, val value: String) : Criteria<T>() {
+        override fun toString(): String {
+            return "${key.name} like $value"
+        }
+    }
+    data class NotLike<T : Any>(val key: KProperty1<T, String?>, val value: String) : Criteria<T>() {
+        override fun toString(): String {
+            return "${key.name} !like $value"
+        }
+    }
 
-    data class In<T : Any, V : Any?>(val key: KProperty1<T, V>, val value: Collection<V>) : Criteria<T>()
-    data class NotIn<T : Any, V : Any?>(val key: KProperty1<T, V>, val value: Collection<V>) : Criteria<T>()
+    data class In<T : Any, V : Any?>(val key: KProperty1<T, V>, val value: Collection<V>) : Criteria<T>() {
+        override fun toString(): String {
+            return "${key.name} in (${value.map { it.toString() }.joinToString { ", " }})"
+        }
+    }
+    data class NotIn<T : Any, V : Any?>(val key: KProperty1<T, V>, val value: Collection<V>) : Criteria<T>() {
+        override fun toString(): String {
+            return "${key.name} not in (${value.map { it.toString() }.joinToString { ", " }})"
+        }
+    }
 
-    data class IsTrue<T : Any>(val key: KProperty1<T, Boolean?>) : Criteria<T>()
-    data class IsFalse<T : Any>(val key: KProperty1<T, Boolean?>) : Criteria<T>()
+    data class IsTrue<T : Any>(val key: KProperty1<T, Boolean?>) : Criteria<T>() {
+        override fun toString(): String {
+            return "${key.name} == true"
+        }
+    }
+    data class IsFalse<T : Any>(val key: KProperty1<T, Boolean?>) : Criteria<T>() {
+        override fun toString(): String {
+            return "${key.name} == false"
+        }
+    }
 }
