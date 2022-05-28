72d5a57adf5734a8b96755a57cb220e9d2e6e6db
siyual.bak <siyual.bak@gmail.com>
2022-05-28 17:23:10 +0900

    test: RefreshTokenAuthorizationStrategyTest

---
+++ domain/user/src/test/kotlin/io/github/siyual_park/user/domain/auth/RefreshTokenAuthorizationStrategyTest.kt
@@ -0,0 +1,47 @@
+package io.github.siyual_park.user.domain.auth
+
+import io.github.siyual_park.auth.domain.authentication.RefreshTokenPayload
+import io.github.siyual_park.auth.domain.token.ClaimEmbedder
+import io.github.siyual_park.auth.domain.token.TokenFactoryProvider
+import io.github.siyual_park.auth.domain.token.TokenMapper
+import io.github.siyual_park.auth.domain.token.TokenStorage
+import io.github.siyual_park.auth.domain.token.TokenTemplate
+import io.github.siyual_park.auth.repository.TokenRepository
+import io.github.siyual_park.user.domain.UserTestHelper
+import io.github.siyual_park.user.dummy.DummyCreateUserPayload
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.time.Duration
+
+class RefreshTokenAuthorizationStrategyTest : UserTestHelper() {
+    private val tokenRepository = TokenRepository(mongoTemplate, eventEmitter)
+
+    private val tokenMapper = TokenMapper(tokenRepository, scopeTokenStorage, eventEmitter)
+    private val claimEmbedder = ClaimEmbedder()
+
+    private val tokenStorage = TokenStorage(tokenRepository, tokenMapper)
+    private val tokenFactoryProvider = TokenFactoryProvider(claimEmbedder, tokenRepository, tokenMapper)
+
+    private val refreshTokenAuthorizationStrategy = RefreshTokenAuthorizationStrategy(tokenStorage)
+
+    init {
+        claimEmbedder.register(UserPrincipal::class, UserPrincipalClaimEmbeddingStrategy())
+    }
+
+    @Test
+    fun authenticate() = blocking {
+        val template = TokenTemplate(type = "test")
+        val tokenFactory = tokenFactoryProvider.get(template)
+
+        val user = DummyCreateUserPayload.create()
+            .let { userFactory.create(it) }
+        val principal = user.toPrincipal()
+
+        val token = tokenFactory.create(principal, Duration.ofMinutes(30))
+
+        assertEquals(
+            principal,
+            refreshTokenAuthorizationStrategy.authenticate(RefreshTokenPayload(token.signature))
+        )
+    }
+}
