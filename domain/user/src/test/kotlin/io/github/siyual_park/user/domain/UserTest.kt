4c2fc939785ca40474e0bb0d82c963f831d4faf0
siyual.bak <siyual.bak@gmail.com>
2022-05-28 13:17:06 +0900

    test: UserTest

--- domain/user/src/test/kotlin/io/github/siyual_park/user/domain/UserTest.kt
+++ domain/user/src/test/kotlin/io/github/siyual_park/user/domain/UserTest.kt
@@ -13,18 +13,22 @@ import io.github.siyual_park.data.test.MongoTestHelper
 import io.github.siyual_park.event.EventEmitter
 import io.github.siyual_park.persistence.loadOrFail
 import io.github.siyual_park.user.dummy.DummyCreateUserPayload
+import io.github.siyual_park.user.dummy.DummyScopeNameFactory
 import io.github.siyual_park.user.migration.CreateUser
 import io.github.siyual_park.user.migration.CreateUserCredential
 import io.github.siyual_park.user.migration.CreateUserScope
 import io.github.siyual_park.user.repository.UserCredentialRepository
 import io.github.siyual_park.user.repository.UserRepository
 import io.github.siyual_park.user.repository.UserScopeRepository
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.flow.toSet
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import java.util.Optional
 
 class UserTest : DataTestHelper() {
     init {
@@ -92,6 +96,81 @@ class UserTest : DataTestHelper() {
 
         assertEquals(other.name, exist.name)
         assertEquals(other.email, exist.email)
+    }
+
+    @Test
+    fun grant() = blocking {
+        val customScope = scopeTokenFactory.upsert(DummyScopeNameFactory.create(10))
+
+        val user1 = DummyCreateUserPayload.create()
+            .let { userFactory.create(it) }
+        val user2 = DummyCreateUserPayload.create()
+            .let { userFactory.create(it) }
+
+        val users = userStorage.load(listOf(user1.id, user2.id)).toList()
+        val loadedUser1 = users.find { it.id == user1.id }
+        val loadedUser2 = users.find { it.id == user2.id }
+
+        assertEquals(loadedUser1, user1)
+        assertEquals(loadedUser2, user2)
+
+        assertEquals(loadedUser1?.getScope(deep = false)?.toSet(), user1.getScope(deep = false).toSet())
+        assertEquals(loadedUser1?.getScope(deep = true)?.toSet(), user1.getScope(deep = true).toSet())
+        assertEquals(loadedUser2?.getScope(deep = false)?.toSet(), user2.getScope(deep = false).toSet())
+        assertEquals(loadedUser2?.getScope(deep = true)?.toSet(), user2.getScope(deep = true).toSet())
+
+        loadedUser1?.grant(customScope)
+
+        assertEquals(loadedUser1?.getScope(deep = false)?.toSet(), user1.getScope(deep = false).toSet())
+        assertEquals(loadedUser1?.getScope(deep = true)?.toSet(), user1.getScope(deep = true).toSet())
+        assertEquals(loadedUser2?.getScope(deep = false)?.toSet(), user2.getScope(deep = false).toSet())
+        assertEquals(loadedUser2?.getScope(deep = true)?.toSet(), user2.getScope(deep = true).toSet())
+
+        loadedUser2?.grant(customScope)
+
+        assertEquals(loadedUser1?.getScope(deep = false)?.toSet(), user1.getScope(deep = false).toSet())
+        assertEquals(loadedUser1?.getScope(deep = true)?.toSet(), user1.getScope(deep = true).toSet())
+        assertEquals(loadedUser2?.getScope(deep = false)?.toSet(), user2.getScope(deep = false).toSet())
+        assertEquals(loadedUser2?.getScope(deep = true)?.toSet(), user2.getScope(deep = true).toSet())
+    }
+
+    @Test
+    fun revoke() = blocking {
+        val customScope = scopeTokenFactory.upsert(DummyScopeNameFactory.create(10))
+        val template = DummyCreateUserPayload.Template(
+            scope = Optional.of(listOf(customScope))
+        )
+
+        val user1 = DummyCreateUserPayload.create(template)
+            .let { userFactory.create(it) }
+        val user2 = DummyCreateUserPayload.create(template)
+            .let { userFactory.create(it) }
+
+        val users = userStorage.load(listOf(user1.id, user2.id)).toList()
+        val loadedUser1 = users.find { it.id == user1.id }
+        val loadedUser2 = users.find { it.id == user2.id }
+
+        assertEquals(loadedUser1, user1)
+        assertEquals(loadedUser2, user2)
+
+        assertEquals(loadedUser1?.getScope(deep = false)?.toSet(), user1.getScope(deep = false).toSet())
+        assertEquals(loadedUser1?.getScope(deep = true)?.toSet(), user1.getScope(deep = true).toSet())
+        assertEquals(loadedUser2?.getScope(deep = false)?.toSet(), user2.getScope(deep = false).toSet())
+        assertEquals(loadedUser2?.getScope(deep = true)?.toSet(), user2.getScope(deep = true).toSet())
+
+        loadedUser1?.revoke(customScope)
+
+        assertEquals(loadedUser1?.getScope(deep = false)?.toSet(), user1.getScope(deep = false).toSet())
+        assertEquals(loadedUser1?.getScope(deep = true)?.toSet(), user1.getScope(deep = true).toSet())
+        assertEquals(loadedUser2?.getScope(deep = false)?.toSet(), user2.getScope(deep = false).toSet())
+        assertEquals(loadedUser2?.getScope(deep = true)?.toSet(), user2.getScope(deep = true).toSet())
+
+        loadedUser2?.revoke(customScope)
+
+        assertEquals(loadedUser1?.getScope(deep = false)?.toSet(), user1.getScope(deep = false).toSet())
+        assertEquals(loadedUser1?.getScope(deep = true)?.toSet(), user1.getScope(deep = true).toSet())
+        assertEquals(loadedUser2?.getScope(deep = false)?.toSet(), user2.getScope(deep = false).toSet())
+        assertEquals(loadedUser2?.getScope(deep = true)?.toSet(), user2.getScope(deep = true).toSet())
     }
 
     companion object {
