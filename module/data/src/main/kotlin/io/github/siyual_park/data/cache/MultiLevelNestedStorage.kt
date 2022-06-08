676084a5abfda2d5853fb30230e1c8d987873a07
siyual.bak <siyual.bak@gmail.com>
2022-06-08 09:20:43 +0900

    refactor: use delegator

--- module/data/src/main/kotlin/io/github/siyual_park/data/cache/MultiLevelNestedStorage.kt
+++ module/data/src/main/kotlin/io/github/siyual_park/data/cache/MultiLevelNestedStorage.kt
@@ -7,7 +7,7 @@ class MultiLevelNestedStorage<ID : Any, T : Any>(
     private val primary: Storage<ID, T>,
     private val secondary: Pool<Storage<ID, T>>,
     private val id: WeekProperty<T, ID?>,
-) : NestedStorage<ID, T> {
+) : NestedStorage<ID, T>, Storage<ID, T> by primary {
     override val parent: NestedStorage<ID, T>? = null
 
     override suspend fun checkout(): Map<ID, T?> {
@@ -32,53 +32,5 @@ class MultiLevelNestedStorage<ID : Any, T : Any>(
                 remove(key)
             }
         }
-    }
-
-    override suspend fun <KEY : Any> createIndex(name: String, property: WeekProperty<T, KEY>) {
-        primary.createIndex(name, property)
-    }
-
-    override suspend fun removeIndex(name: String) {
-        primary.removeIndex(name)
-    }
-
-    override suspend fun getIndexes(): Map<String, WeekProperty<T, *>> {
-        return primary.getIndexes()
-    }
-
-    override suspend fun containsIndex(name: String): Boolean {
-        return primary.containsIndex(name)
-    }
-
-    override suspend fun <KEY : Any> getIfPresent(index: String, key: KEY, loader: suspend () -> T?): T? {
-        return primary.getIfPresent(index, key, loader)
-    }
-
-    override suspend fun <KEY : Any> getIfPresent(index: String, key: KEY): T? {
-        return primary.getIfPresent(index, key)
-    }
-
-    override suspend fun getIfPresent(id: ID, loader: suspend () -> T?): T? {
-        return primary.getIfPresent(id, loader)
-    }
-
-    override suspend fun getIfPresent(id: ID): T? {
-        return primary.getIfPresent(id)
-    }
-
-    override suspend fun remove(id: ID) {
-        primary.remove(id)
-    }
-
-    override suspend fun add(entity: T) {
-        primary.add(entity)
-    }
-
-    override suspend fun clear() {
-        primary.clear()
-    }
-
-    override suspend fun entries(): Set<Pair<ID, T>> {
-        return primary.entries()
     }
 }
