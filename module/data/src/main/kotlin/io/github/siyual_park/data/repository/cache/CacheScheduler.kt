6df84f7b0d086475e2484ecd99bb1a58aaae7049
siyual.bak <siyual.bak@gmail.com>
2022-06-09 10:22:19 +0900

    feat: cache schedule

---
+++ module/data/src/main/kotlin/io/github/siyual_park/data/repository/cache/CacheScheduler.kt
@@ -0,0 +1,77 @@
+package io.github.siyual_park.data.repository.cache
+
+import java.time.Duration
+import java.time.Instant
+import java.util.concurrent.atomic.AtomicLong
+import kotlin.math.abs
+import kotlin.math.ceil
+import kotlin.math.floor
+
+class CacheScheduler {
+    val cacheSamples = List(5) { AtomicLong(0) }
+    val nonCacheSamples = List(5) { AtomicLong(0) }
+
+    private val samplingMin = 1L
+    private val samplingMax = 150L
+
+    fun useCache(size: Int): Boolean {
+        return getSample(size, cacheSamples) <= getSample(size, nonCacheSamples)
+    }
+
+    private fun getSample(size: Int, sample: List<AtomicLong>): Long {
+        val (firstSample, secondSample) = getSamples(size, sample)
+        val (firstValue, fistRate) = firstSample
+        val (secondValue, secondRate) = secondSample
+
+        val result = firstValue.get() * fistRate + secondValue.get() + secondRate
+        if (result == 0L) {
+            return Long.MAX_VALUE
+        }
+        return result
+    }
+
+    fun getSamples(size: Int, sample: List<AtomicLong>): Pair<Pair<AtomicLong, Long>, Pair<AtomicLong, Long>> {
+        if (size <= samplingMin) {
+            return (sample[0] to 100L) to (sample[1] to 0L)
+        }
+        if (size >= samplingMax) {
+            return (sample[sample.size - 2] to 0L) to (sample[sample.size - 1] to 100L)
+        }
+
+        val unit = (samplingMax - samplingMin) / sample.size
+
+        val current = ((size - samplingMin).toDouble() / unit)
+
+        val first = floor(current).toInt()
+        val second = ceil(current).toInt()
+
+        return (sample[first] to (abs(1 - (current - first)) * 100).toLong()) to (sample[second] to (abs(1 - (current - second)) * 100).toLong())
+    }
+}
+
+inline fun <T> CacheScheduler.measureCache(size: Int, action: () -> T): T {
+    return measure(size, cacheSamples, action)
+}
+
+inline fun <T> CacheScheduler.measureNonCache(size: Int, action: () -> T): T {
+    return measure(size, nonCacheSamples, action)
+}
+
+inline fun <T> CacheScheduler.measure(size: Int, sample: List<AtomicLong>, action: () -> T): T {
+    val start = Instant.now()
+    try {
+        return action()
+    } finally {
+        val end = Instant.now()
+        val diff = Duration.between(start, end)
+
+        val unit = diff.toMillis() / size
+
+        val (firstSample, secondSample) = getSamples(size, sample)
+        val (firstValue, fistRate) = firstSample
+        val (secondValue, secondRate) = secondSample
+
+        firstValue.updateAndGet { (it * (100 - fistRate * 25 / 100) + unit * fistRate * 25 / 100) / 100 }
+        secondValue.updateAndGet { (it * (100 - secondRate * 25 / 100) + unit * secondRate * 25 / 100) / 100 }
+    }
+}
--- module/data/src/main/kotlin/io/github/siyual_park/data/repository/cache/SimpleCachedRepository.kt
+++ module/data/src/main/kotlin/io/github/siyual_park/data/repository/cache/SimpleCachedRepository.kt
@@ -17,6 +17,7 @@ class SimpleCachedRepository<T : Any, ID : Any>(
     private val storage: Storage<ID, T>,
     private val id: WeekProperty<T, ID?>
 ) : Repository<T, ID> {
+    private val cacheScheduler = CacheScheduler()
 
     override suspend fun create(entity: T): T {
         return delegator.create(entity)
@@ -55,7 +56,17 @@ class SimpleCachedRepository<T : Any, ID : Any>(
     }
 
     override suspend fun findById(id: ID): T? {
-        return storage.getIfPresent(id) { delegator.findById(id) }
+        return if (cacheScheduler.useCache(1)) {
+            cacheScheduler.measureCache(1) {
+                storage.getIfPresent(id) {
+                    delegator.findById(id)
+                }
+            }
+        } else {
+            cacheScheduler.measureNonCache(1) {
+                delegator.findById(id)
+            }
+        }
     }
 
     override fun findAll(): Flow<T> {
@@ -68,34 +79,49 @@ class SimpleCachedRepository<T : Any, ID : Any>(
     }
 
     override fun findAllById(ids: Iterable<ID>): Flow<T> {
-        return flow {
-            val result = mutableListOf<T>()
-            val notCachedIds = mutableListOf<ID>()
-            ids.forEach { id ->
-                val cached = storage.getIfPresent(id)
-                if (cached == null) {
-                    notCachedIds.add(id)
-                } else {
-                    result.add(cached)
+        val size = ids.count()
+        return if (cacheScheduler.useCache(size)) {
+            flow {
+                cacheScheduler.measureCache(size) {
+                    val result = mutableListOf<T>()
+                    val notCachedIds = mutableListOf<ID>()
+
+                    ids.forEach { id ->
+                        val cached = storage.getIfPresent(id)
+                        if (cached == null) {
+                            notCachedIds.add(id)
+                        } else {
+                            result.add(cached)
+                        }
+                    }
+
+                    if (notCachedIds.isNotEmpty()) {
+                        delegator.findAllById(notCachedIds)
+                            .onEach { storage.add(it) }
+                            .collect { result.add(it) }
+                    }
+
+                    emitAll(
+                        result.also {
+                            it.sortWith { p1, p2 ->
+                                val p1Id = id.get(p1)
+                                val p2Id = id.get(p2)
+
+                                ids.indexOf(p1Id) - ids.indexOf(p2Id)
+                            }
+                        }.asFlow()
+                    )
                 }
             }
-
-            if (notCachedIds.isNotEmpty()) {
-                delegator.findAllById(notCachedIds)
-                    .onEach { storage.add(it) }
-                    .collect { result.add(it) }
+        } else {
+            flow {
+                cacheScheduler.measureNonCache(size) {
+                    emitAll(
+                        delegator.findAllById(ids)
+                            .onEach { storage.add(it) }
+                    )
+                }
             }
-
-            emitAll(
-                result.also {
-                    it.sortWith { p1, p2 ->
-                        val p1Id = id.get(p1)
-                        val p2Id = id.get(p2)
-
-                        ids.indexOf(p1Id) - ids.indexOf(p2Id)
-                    }
-                }.asFlow()
-            )
         }
     }
 
