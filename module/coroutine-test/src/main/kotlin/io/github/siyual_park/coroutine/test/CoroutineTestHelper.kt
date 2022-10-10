7842e878b8aabcfbfcb95dcc57b8cf8554d7cc04
siyual.bak <siyual.bak@gmail.com>
2022-10-10 23:39:38 +0900

    chore: use Dispatchers.Default

--- module/coroutine-test/src/main/kotlin/io/github/siyual_park/coroutine/test/CoroutineTestHelper.kt
+++ module/coroutine-test/src/main/kotlin/io/github/siyual_park/coroutine/test/CoroutineTestHelper.kt
@@ -2,34 +2,22 @@ package io.github.siyual_park.coroutine.test
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.setMain
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 
-@Suppress("OPT_IN_USAGE")
 abstract class CoroutineTestHelper {
-    private val mainThreadSurrogate = newSingleThreadContext("Test thread")
-
     @BeforeEach
     open fun setUp() {
-        Dispatchers.setMain(mainThreadSurrogate)
     }
 
     @AfterEach
     open fun tearDown() {
-        Dispatchers.resetMain()
-        mainThreadSurrogate.close()
     }
 
     fun blocking(func: suspend CoroutineScope.() -> Unit) {
-        runBlocking {
-            launch(Dispatchers.Main) {
-                func(this)
-            }
+        runBlocking(Dispatchers.Default) {
+            func(this)
         }
     }
 }
