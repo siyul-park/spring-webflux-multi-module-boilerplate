f66f6430076db9de9dc81d25b497e81b33a64005
siyual.bak <siyual.bak@gmail.com>
2022-10-10 16:34:12 +0900

    refactor: Remove unnecessary import

--- module/coroutine-test/src/main/kotlin/io/github/siyual_park/coroutine/test/CoroutineTestHelper.kt
+++ module/coroutine-test/src/main/kotlin/io/github/siyual_park/coroutine/test/CoroutineTestHelper.kt
@@ -2,8 +2,6 @@ package io.github.siyual_park.coroutine.test
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.ObsoleteCoroutinesApi
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.newSingleThreadContext
 import kotlinx.coroutines.runBlocking
@@ -12,8 +10,7 @@ import kotlinx.coroutines.test.setMain
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 
-@Suppress("EXPERIMENTAL_IS_NOT_ENABLED")
-@OptIn(ExperimentalCoroutinesApi::class, ObsoleteCoroutinesApi::class)
+@Suppress("OPT_IN_USAGE")
 abstract class CoroutineTestHelper {
     private val mainThreadSurrogate = newSingleThreadContext("Test thread")
 
